63b360a7cf120c280d7b7e36b547168e9c881919
Shreyash Singh <111888888+Shreyash-Riushka@users.noreply.github.com>
2025-10-26 16:01:00 +0530

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -21,23 +21,19 @@ const Contact = () => {
           className={`flex flex-col items-start justify-center ${monaSans.className} relative w-full sm:items-center lg:max-w-[1440px] `}
         >
           <AnimatedWords2
-            title={"Let's Talk"}
+            title={"Let's Connect"}
             style={
               "flex max-w-[500px] flex-col items-start text-left text-[150px] font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center sm:text-[170px] md:text-[200px] lg:text-center lg:text-[270px] xl:text-[390px]"
             }
           />
-          <Image
-            src={heartIcon}
-            alt="Heart Icon"
-            className="heartbeat md:-bottom-18 absolute -bottom-5 left-64 w-[120px] sm:-bottom-14 sm:left-[40%] md:left-[40%] md:w-[150px] lg:-bottom-16 lg:left-[42%] lg:w-[230px]"
-          />
+      
         </div>
 
         <div className="mt-20 flex w-full flex-col items-end justify-center gap-16 sm:mt-32 sm:gap-12 md:mt-40 md:flex-row md:items-start md:justify-between lg:mt-12 lg:max-w-[1440px]">
           <div className=" flex w-[350px] max-w-[90%] flex-col items-end text-right text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[350px] sm:text-[14px] md:w-[310px] md:items-start md:text-left md:text-[16px] lg:w-[420px] lg:text-[16px]">
             <AnimatedBody
               text={
-                "Got a question, proposal, project, or want to work together on something?"
+                "Want to create something awesome together? Hit me up if you’ve got something exciting to work on!"
               }
               className={
                 "-mb-1 inline-block overflow-hidden pt-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
@@ -45,7 +41,7 @@ const Contact = () => {
             />
             <div className="bor der mt-5 flex w-[298px] items-center gap-1 md:w-[335px] md:gap-2.5">
               <Link
-                href="mailto:victor.will.chuk@gmail.com?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
+                href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnMhSswsWrDDggrSlNTbRVwbnTbDTwLlhMCkwZRBWHzNCszTLtPtBxbmvhSDLddJXBDvV"
                 target="_blank"
                 aria-label="Send me an email"
                 className="bor der mt-1 w-[147px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[170px] md:mt-3 lg:mt-4"
@@ -62,7 +58,7 @@ const Contact = () => {
                 }
               />
               <Link
-                href="https://cal.com/victorwilliams/30min"
+                href="https://cal.com/shreyash-kumar-singh/30min"
                 target="_blank"
                 aria-label="Send me an email"
                 className="bor der mt-1 w-[110px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:-ml-[3px] md:w-[120px] lg:mt-4"
@@ -77,12 +73,12 @@ const Contact = () => {
 
           <div className="flex gap-10 text-[16px] font-bold text-[#e4ded7]  sm:gap-14 sm:text-[24px] md:gap-10 md:text-[16px] lg:gap-20 lg:text-[28px]">
             <Link
-              href="https://github.com/victorcodess"
+              href="https://github.com/Shreyash-Riushka"
               target="_blank"
               aria-label="View GitHub Profile"
             >
               <AnimatedTitle
-                text={"GH"}
+                text={"GITHUB"}
                 className={
                   "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
@@ -91,12 +87,12 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://www.linkedin.com/in/victor-williams-chukwudi/"
+              href="https://www.linkedin.com/in/shreyash-s-944462290/"
               target="_blank"
               aria-label="View LinkedIn Profile"
             >
               <AnimatedTitle
-                text={"LN"}
+                text={"LINKEDIN"}
                 className={
                   "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
@@ -105,12 +101,12 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://twitter.com/victorwill__"
+              href="https://x.com/RiushkaShr86175"
               target="_blank"
               aria-label="View Twitter Profile"
             >
               <AnimatedTitle
-                text={"TW"}
+                text={"TWITTER"}
                 className={
                   "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
@@ -119,12 +115,12 @@ const Contact = () => {
               />
             </Link>
             <Link
-              href="https://www.instagram.com/victorwill__/"
+              href="https://www.instagram.com/shreyashriushka?igsh=MXg1YXVpbDB0Z3RvMA=="
               target="_blank"
               aria-label="View Instagram Profile"
             >
               <AnimatedTitle
-                text={"IG"}
+                text={"INSTAGRAM"}
                 className={
                   "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
@@ -137,13 +133,7 @@ const Contact = () => {
               target="_blank"
               aria-label="View Hashnode Profile"
             >
-              <AnimatedTitle
-                text={"HN"}
-                className={
-                  "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
-                }
-                wordSpace={"mr-[0.25em]"}
-                charSpace={"mr-[0.01em]"}
+            
               />
             </Link>
           </div>
