c9c74f05837d7bf2c8e0d648f3e312cb0812ad89
Shreyash Singh <111888888+Shreyash-Riushka@users.noreply.github.com>
2025-11-11 16:48:47 -0800

    Update projectDetails.ts

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -29,10 +29,43 @@ export const devProjects = [
     technologies: ["FIGMA", "ADOBE XD", "Framer Motion"],
     github: "https://github.com/victorcodess/interlock",
     demo: "https://interlock-teal.vercel.app/",
+    image: require(".//../../public/projects/interlock.png"),
     image: require(".//../../public/projects/oyo1.jpg"),
     available: true,
   },
-
+  {
+    id: 2,
+    name: "Synthetix",
+    description:
+      "Built specifically for an AI startup, this website lets them present cutting-edge AI data processing solutions tailored to their customers' needs.",
+    technologies: ["React", "Next.js", "Prismic CMS"],
+    github: "https://github.com/victorcodess/synthetix",
+    demo: "https://synthetix-iota.vercel.app/",
+    image: require(".//../../public/projects/synthetix-flip.png"),
+    available: true,
+  },
+  {
+    id: 3,
+    name: "Propellent",
+    description:
+      "A website built for a software startup and small business, to showcase their services and mark their online presence.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion"],
+    github: "https://github.com/victorcodess/propellent",
+    demo: "https://propellent.vercel.app/",
+    image: require(".//../../public/projects/propellent-new.png"),
+    available: true,
+  },
+  {
+    id: 4,
+    name: "Flixify",
+    description:
+      "Flixify lets you seamlessly explore movies and TV series, add bookmarks, and search across all pages. It offers user authentication along with a theme switch.",
+    technologies: ["Next.js", "Typescript", "Prisma"],
+    github: "https://github.com/victorcodess/flixify",
+    demo: "https://flixify.victorwilliams.me/",
+    image: require(".//../../public/projects/flixify.png"),
+    available: true,
+  },
   // {
   //   id: 4,
   //   name: "SkyWatch",
@@ -89,4 +122,52 @@ export const devProjects = [
   //   image: require(".//../../public/projects/mlsc.png"),
   //   available: false,
   // },
+];
+
+export const designProjects = [
+  {
+    id: 1,
+    name: "Hebron Statup Lab Website",
+    description:
+      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 2,
+    name: "RAGS Scrubs Website",
+    description:
+      "An image generator website that allows users to generate, combine, and download images.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 3,
+    name: "Crown Branding Agency Website",
+    description:
+      "A website that reduces the length of your URL using Bit.ly's API",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image:
+      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 4,
+    name: "Titi Mobile App",
+    description:
+      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
+    available: false,
+  },
 ];
